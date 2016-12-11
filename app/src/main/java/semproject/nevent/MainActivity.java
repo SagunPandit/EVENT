e47638fe57a0ff88dcb0bff3e4b7fd5c3c0e1d1d
SagunPandit <sagunp68@gmail.com>
2016-12-11 21:37:08 +0545

    Check Commit

--- app/src/main/java/semproject/nevent/MainActivity.java
+++ app/src/main/java/semproject/nevent/MainActivity.java
@@ -1,7 +1,12 @@
 package semproject.nevent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void signup(View view){
+        Intent i= new Intent(this, SignUp.class);
+        startActivity(i);
+    }
+
+    public void signin(View view){
+        String toastMesg = "Comming Soon!!!!";
+        Toast toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
+        TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+        if (v != null) v.setGravity(Gravity.CENTER);
+        toast.show();
     }
 }
---
+++ app/src/main/java/semproject/nevent/SignIn.java
@@ -0,0 +1,10 @@
+package semproject.nevent;
+
+/**
+ * Created by User on 12/11/2016.
+ */
+
+public class SignIn {
+    public SignIn() {
+    }
+}
