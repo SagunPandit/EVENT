e90d6ef41ee937f0031f1ea8e4c096e3bac287c8
SagunPandit <sagunp68@gmail.com>
2016-12-16 20:23:59 +0545

    Database Sample

--- app/src/main/java/semproject/nevent/MainActivity.java
+++ app/src/main/java/semproject/nevent/MainActivity.java
@@ -1,15 +1,25 @@
 package semproject.nevent;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
-    /*CommitCheck*/
+    final String STRING_TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +31,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signin(View view){
-        String toastMesg = "Comming Soon!!!!";
-        Toast toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
-        TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-        if (v != null) v.setGravity(Gravity.CENTER);
-        toast.show();
+        final EditText username= (EditText) findViewById(R.id.username_main);
+        final EditText password= (EditText) findViewById(R.id.password_main);
+
+        Response.Listener<String> responseListener= new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+
+                    JSONObject jsonObject=new JSONObject(response);
+                    boolean success = jsonObject.getBoolean("success");
+                    if(success){
+                        String username= jsonObject.getString("username");
+                        String email= jsonObject.getString("email");
+                        Intent intent= new Intent(MainActivity.this,HomePage.class);
+                        intent.putExtra("username",username);
+                        intent.putExtra("email",email);
+                        startActivity(intent);
+                    }
+                    else {
+                        AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage("Login Failed")
+                                .setNegativeButton("Retry",null)
+                                .create()
+                                .show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        if((username.getText().toString().isEmpty()|| password.getText().toString().isEmpty())){
+            String toastMesg = "Please enter username and password to login";
+            Toast toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
+            TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+            if (v != null) v.setGravity(Gravity.CENTER);
+            toast.show();
+        }
+        else{
+            LoginRequest loginRequest=new LoginRequest(username.getText().toString(),password.getText().toString(), responseListener);
+            RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+            queue.add(loginRequest);
+        }
+
     }
 }
--- app/src/main/java/semproject/nevent/SignUp.java
+++ app/src/main/java/semproject/nevent/SignUp.java
@@ -1,25 +1,116 @@
 package semproject.nevent;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class SignUp extends AppCompatActivity {
+    final String STRING_TAG = "SignUp";
+    EditText username, password, email, confpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        username = (EditText) findViewById(R.id.username_register);
+        password = (EditText) findViewById(R.id.password_register);
+        email = (EditText) findViewById(R.id.email_register);
+        confpassword = (EditText) findViewById(R.id.confpassword_register);
     }
 
-    public void signup(View view){
-        String toastMesg = "Comming Soon!!!!";
-        Toast toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
-        TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-        if (v != null) v.setGravity(Gravity.CENTER);
-        toast.show();
+    public void signup(View view) {
+
+        if (!(password.getText().toString()).equals(confpassword.getText().toString())) {
+            String toastMesg = "Please enter your password correctly.";
+            Toast toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
+            TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+            if (v != null) v.setGravity(Gravity.CENTER);
+            toast.show();
+        }
+        else {
+            final String fusername = username.getText().toString();
+            final String femail = email.getText().toString();
+            final String fpassword = password.getText().toString();
+
+            Response.Listener<String> responseListener = new Response.Listener<String>() {
+
+                @Override
+                public void onResponse(String response) {
+                    String toastMesg;
+                    Toast toast;
+                    TextView v;
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        boolean success = jsonResponse.getBoolean("success");
+                        /* int success = jsonResponse.getInt("success");
+                           Log.e(STRING_TAG,Integer.toString(success));
+                           switch (success){
+                            case 0:
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
+                                builder.setMessage("Register Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                                break;
+                            case 1:
+                                Log.e(STRING_TAG, "Success");
+                                Intent intent = new Intent(SignUp.this, MainActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 2:
+                                toastMesg = "This email is already registered";
+                                toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
+                                v = (TextView) toast.getView().findViewById(android.R.id.message);
+                                if (v != null) v.setGravity(Gravity.CENTER);
+                                toast.show();
+                                break;
+                            case 3:
+                                toastMesg = "This username is already taken.";
+                                toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
+                                v = (TextView) toast.getView().findViewById(android.R.id.message);
+                                if (v != null) v.setGravity(Gravity.CENTER);
+                                toast.show();
+                                break;
+                            default:
+                                break;
+                        }*/
+                        if (success) {
+                            Log.e(STRING_TAG, "Success");
+                            Intent intent = new Intent(SignUp.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
+                            builder.setMessage("Register Failed")
+                                    .setNegativeButton("Retry", null)
+                                    .create()
+                                    .show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            RegisterRequest registerRequest = new RegisterRequest(fusername, femail, fpassword, responseListener);
+            RequestQueue queue = Volley.newRequestQueue(SignUp.this);
+            queue.add(registerRequest);
+        }
+
     }
 }
+
