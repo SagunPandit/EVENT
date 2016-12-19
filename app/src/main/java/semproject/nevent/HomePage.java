474559dbbe58afcf2e0b8ede128a65bd96e9b8c4
aayushsthapit <aayushratnasthapit@gmail.com>
2016-12-19 12:35:06 +0545

    Signup and signin complete

--- app/src/main/java/semproject/nevent/HomePage.java
+++ app/src/main/java/semproject/nevent/HomePage.java
@@ -1,13 +1,130 @@
 package semproject.nevent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    NavigationView navigationView=null;
+    Toolbar toolbar=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        // Set the fragments
+        Trending trending=new Trending();
+        android.support.v4.app.FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, trending);
+        fragmentTransaction.commit();
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed(); //use property of super class - Appcompat actvity
+        }
+    }
+    public void gopage(View view) {
+        Intent i=new Intent(this, HomePage.class);
+        startActivity(i);
+
+    }
+
+
+
+
+
+
+
+
+
+    @Override //http://stackoverflow.com/questions/31231609/creating-a-button-in-android-toolbar
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.homepage, menu);
+        return true;
+    }
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_sports) {
+            Sports sports=new Sports();
+            android.support.v4.app.FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, sports);
+            fragmentTransaction.commit();
+
+            // Handle the camera action
+        } else if (id == R.id.nav_parties) {
+            Parties parties=new Parties();
+            android.support.v4.app.FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, parties);
+            fragmentTransaction.commit();
+
+
+        } else if (id == R.id.nav_conferences) {
+            Conference conference=new Conference();
+            android.support.v4.app.FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, conference);
+            fragmentTransaction.commit();
+
+
+        } else if (id == R.id.nav_donations) {
+            Donations donations=new Donations();
+            android.support.v4.app.FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, donations);
+            fragmentTransaction.commit();
+
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/semproject/nevent/SignUp.java
+++ app/src/main/java/semproject/nevent/SignUp.java
@@ -39,96 +39,109 @@ public class SignUp extends AppCompatActivity {
     }
 
     public void signup(View view) {
-
-        if (!(password.getText().toString()).equals(confpassword.getText().toString())) {
-            String toastMesg = "Please enter your password correctly.";
-            Toast toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
-            TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+        String toastMesg;
+        Toast toast;
+        TextView v;
+        if(password.getText().toString().isEmpty() || confpassword.getText().toString().isEmpty() || username.getText().toString().isEmpty() || email.getText().toString().isEmpty()){
+            toastMesg = "All fields must be filled.";
+            toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
+            v = (TextView) toast.getView().findViewById(android.R.id.message);
             if (v != null) v.setGravity(Gravity.CENTER);
             toast.show();
-        }
-        else {
-            final String fusername = username.getText().toString();
-            final String femail = email.getText().toString();
-            final String fpassword = password.getText().toString();
+        }else {
+            if (!(password.getText().toString()).equals(confpassword.getText().toString())) {
+                toastMesg = "Please enter your password correctly.";
+                toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
+                v = (TextView) toast.getView().findViewById(android.R.id.message);
+                if (v != null) v.setGravity(Gravity.CENTER);
+                toast.show();
+            } else {
+                final String fusername = username.getText().toString();
+                final String femail = email.getText().toString();
+                final String fpassword = password.getText().toString();
 
-            Response.Listener<String> responseListener = new Response.Listener<String>() {
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
 
-                @Override
-                public void onResponse(String response) {
-                    String toastMesg;
-                    Toast toast;
-                    TextView v;
-                    try {
-                        Log.e(STRING_TAG, "Try");
-                        JSONObject jsonResponse = new JSONObject(response);
+                    @Override
+                    public void onResponse(String response) {
+                        String toastMesg;
+                        Toast toast;
+                        TextView v;
+                        try {
+                            Log.e(STRING_TAG, "Try");
+                            JSONObject jsonResponse = new JSONObject(response);
 
-                        /*int success = jsonResponse.getInt("success");
-                        Log.e(STRING_TAG,Integer.toString(success));
-                        switch (success) {
-                            case 0:
+                            int success = jsonResponse.getInt("success");
+                            Log.e(STRING_TAG,Integer.toString(success));
+                            switch (success) {
+                                case 0:
+                                    toastMesg = "Enter valid email format";
+                                    toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
+                                    v = (TextView) toast.getView().findViewById(android.R.id.message);
+                                    if (v != null) v.setGravity(Gravity.CENTER);
+                                    toast.show();
+                                    break;
+                                case 1:
+                                    Log.e(STRING_TAG, "Success");
+                                    Intent intent = new Intent(SignUp.this, MainActivity.class);
+                                    startActivity(intent);
+                                    toastMesg = "Congratulations!! You have been registered successfully.";
+                                    toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_LONG);
+                                    v = (TextView) toast.getView().findViewById(android.R.id.message);
+                                    if (v != null) v.setGravity(Gravity.CENTER);
+                                    toast.show();
+                                    break;
+                                case 2:
+                                    toastMesg = "This email is already registered";
+                                    toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
+                                    v = (TextView) toast.getView().findViewById(android.R.id.message);
+                                    if (v != null) v.setGravity(Gravity.CENTER);
+                                    toast.show();
+                                    break;
+                                case 3:
+                                    toastMesg = "This username is already taken.";
+                                    toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
+                                    v = (TextView) toast.getView().findViewById(android.R.id.message);
+                                    if (v != null) v.setGravity(Gravity.CENTER);
+                                    toast.show();
+                                    break;
+                                default:
+                                    break;
+                            }
+                            /*boolean success = jsonResponse.getBoolean("success");
+
+                            if (success) {
+                                Log.e(STRING_TAG, "Successs " + Boolean.toString(jsonResponse.getBoolean("emailerror")));
+                                if (jsonResponse.getBoolean("emailerror")) {
+                                    toastMesg = "Enter valid email format";
+                                    toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
+                                    v = (TextView) toast.getView().findViewById(android.R.id.message);
+                                    if (v != null) v.setGravity(Gravity.CENTER);
+                                    toast.show();
+                                } else {
+
+                                    Intent intent = new Intent(SignUp.this, MainActivity.class);
+                                    startActivity(intent);
+                                }
+
+                            } else {
                                 AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
                                 builder.setMessage("Register Failed")
                                         .setNegativeButton("Retry", null)
                                         .create()
                                         .show();
-                                break;
-                            case 1:
-                                Log.e(STRING_TAG, "Success");
-                                Intent intent = new Intent(SignUp.this, MainActivity.class);
-                                startActivity(intent);
-                                break;
-                            case 2:
-                                toastMesg = "This email is already registered";
-                                toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
-                                v = (TextView) toast.getView().findViewById(android.R.id.message);
-                                if (v != null) v.setGravity(Gravity.CENTER);
-                                toast.show();
-                                break;
-                            case 3:
-                                toastMesg = "This username is already taken.";
-                                toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
-                                v = (TextView) toast.getView().findViewById(android.R.id.message);
-                                if (v != null) v.setGravity(Gravity.CENTER);
-                                toast.show();
-                                break;
-                            default:
-                                break;
-                        }*/
-                        boolean success = jsonResponse.getBoolean("success");
+                            }*/
 
-                        if (success) {
-
-                            if(jsonResponse.getBoolean("emailerror")){
-                                toastMesg = "Enter valid email format";
-                                toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
-                                v = (TextView) toast.getView().findViewById(android.R.id.message);
-                                if (v != null) v.setGravity(Gravity.CENTER);
-                                toast.show();
-                            }
-                            else {
-
-                                Intent intent = new Intent(SignUp.this, MainActivity.class);
-                                startActivity(intent);
-                            }
-
-                        } else {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
-                            builder.setMessage("Register Failed")
-                                    .setNegativeButton("Retry", null)
-                                    .create()
-                                    .show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
                     }
-                }
-            };
-            RegisterRequest registerRequest = new RegisterRequest(fusername, femail, fpassword, responseListener);
-            RequestQueue queue = Volley.newRequestQueue(this);
-            queue.add(registerRequest); //automatically start the string request on the queue
+                };
+                RegisterRequest registerRequest = new RegisterRequest(fusername, femail, fpassword, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(this);
+                queue.add(registerRequest); //automatically start the string request on the queue
+            }
         }
-
     }
 }
+
