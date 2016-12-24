e0c5a66561d1faa7935248fe7256507af89b9c10
SagunPandit <sagunp68@gmail.com>
2016-12-24 20:14:58 +0545

    Added Splash Screen

--- app/src/main/java/semproject/nevent/MainActivity.java
+++ app/src/main/java/semproject/nevent/MainActivity.java
@@ -1,6 +1,8 @@
 package semproject.nevent;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,22 +20,32 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import static com.google.android.gms.common.internal.safeparcel.SafeParcelable.NULL;
+
 public class MainActivity extends AppCompatActivity {
     final String STRING_TAG = "MainActivity";
+    public static String PreferenceFile = "neventpreff" ;
+    SharedPreferences sharedpreferences;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void register(View view){
-        Intent i= new Intent(this, SignUp.class);
-        startActivity(i);
+        sharedpreferences = getSharedPreferences(PreferenceFile, Context.MODE_PRIVATE);
+        String username= sharedpreferences.getString("username",NULL);
+        String password= sharedpreferences.getString("password",NULL);
+        if(username.isEmpty()|| password.isEmpty()){
+            Intent intent= new Intent(MainActivity.this,SignIn.class);
+            startActivity(intent);
+            finish();
+        }
+        else{
+            listenerFunction(username,password);
+        }
     }
 
-    public void signin(View view){
-        final EditText username= (EditText) findViewById(R.id.username_main);
-        final EditText password= (EditText) findViewById(R.id.password_main);
-
+    public void listenerFunction(String username, String password){
         Response.Listener<String> responseListener= new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -44,13 +56,22 @@ public class MainActivity extends AppCompatActivity {
                     boolean success = jsonObject.getBoolean("success");
                     Log.e(STRING_TAG,jsonObject.getString("inside"));
                     if(success){
-
                         String username= jsonObject.getString("username");
                         String email= jsonObject.getString("email");
+                        String password = jsonObject.getString("password");
+
+                        //using sharedpreference to store username and password
+                        sharedpreferences = getSharedPreferences(PreferenceFile, Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                        editor.putString("username", username);
+                        editor.putString("password", password);
+                        editor.apply();
+
                         Intent intent= new Intent(MainActivity.this,HomePage.class);
                         intent.putExtra("username",username);
                         intent.putExtra("email",email);
                         startActivity(intent);
+                        finish();
                     }
                     else {
                         AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
@@ -64,19 +85,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-
-        if((username.getText().toString().isEmpty()|| password.getText().toString().isEmpty())){
-            String toastMesg = "Please enter username and password to login";
-            Toast toast = Toast.makeText(getApplicationContext(), toastMesg, Toast.LENGTH_SHORT);
-            TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-            if (v != null) v.setGravity(Gravity.CENTER);
-            toast.show();
-        }
-        else{
-            LoginRequest loginRequest=new LoginRequest(username.getText().toString(),password.getText().toString(), responseListener);
-            RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-            queue.add(loginRequest);
-        }
-
+        LoginRequest loginRequest=new LoginRequest(username, password, responseListener);
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        queue.add(loginRequest);
     }
 }
